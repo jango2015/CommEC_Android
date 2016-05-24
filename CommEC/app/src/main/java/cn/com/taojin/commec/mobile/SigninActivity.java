de26b1279dac07cc551afc19c3cd0b404c6b5dfc
kris.li <kris.li@iisigroup.com>
2016-05-24 17:35:23 +0800

    Modified M3.0.0 search

---
+++ CommEC/app/src/main/java/cn/com/taojin/commec/mobile/SigninActivity.java
@@ -0,0 +1,16 @@
+package cn.com.taojin.commec.mobile;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Kris on 2016/05/24.
+ */
+public class SigninActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
