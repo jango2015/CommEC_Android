f7c8a16b82f9f73aec62e4d1bd81ee449b10213d
kris.li <kris.li@iisigroup.com>
2016-05-27 17:36:23 +0800

    Added M3.1.1 Search babe and extra added M3.1.2 search shop content layout.

--- CommEC/app/src/main/java/cn/com/taojin/commec/mobile/UItestActivity.java
+++ CommEC/app/src/main/java/cn/com/taojin/commec/mobile/UItestActivity.java
@@ -8,7 +8,7 @@ public class UItestActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_message);
+        setContentView(R.layout.content_search_babe);
 
 
     }
