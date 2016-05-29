05b4adea532c4e187299fa544f3407a06cbd490e
kris.li <kris.li@iisigroup.com>
2016-05-29 13:52:04 +0800

    Modified M3.2.1 and M3.2.2.

--- CommEC/app/src/main/java/cn/com/taojin/commec/mobile/UItestActivity.java
+++ CommEC/app/src/main/java/cn/com/taojin/commec/mobile/UItestActivity.java
@@ -8,7 +8,7 @@ public class UItestActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.content_search_goods_detail);
+        setContentView(R.layout.activity_shop_detail);
 
     }
 }
